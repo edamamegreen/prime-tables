1cbc8d8d1b1887234b5798ad9031ab85ee0a004f
edamamegreen <hnvance@gmail.com>
2019-01-06 20:55:30 -0800

    Add more test cases for generate-primes.

--- test/fccode/core_test.clj
+++ test/fccode/core_test.clj
@@ -3,6 +3,8 @@
             [fccode.core :refer :all]))
 
 (deftest test-generate-primes
-  (testing "Generate primes"
+  (testing "Generate primes function"
+    (is (= nil (generate-primes -1)))
+    (is (= nil (generate-primes 0)))
     (is (= '(1) (generate-primes 1)))
     (is (= '(1 2 3 5) (generate-primes 4)))))
